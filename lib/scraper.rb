d0ad4a7e75219b496c8dcd935aea04911b8185d5
Asha Fernandez <ash.fernan90@gmail.com>
2020-02-06 02:18:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
 
+doc.css(".headline-26OIBN").text
+
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
